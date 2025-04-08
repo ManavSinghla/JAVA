8582e43b2b462ddddd43d9b3ed7dfa03ab7f785b
Manav Singla <manavsinghla123@gmail.com>
2025-04-08 09:39:24 +0530

    08-04-25

--- AssertionExample.java
+++
@@ -1,7 +0,0 @@
-public class AssertionExample {
-    public static void main(String[] args) {
-        int age = 15;
-        assert age>=18:"Age must be 18 or above!";
-        System.out.println("Age is valid");
-    }
-}
